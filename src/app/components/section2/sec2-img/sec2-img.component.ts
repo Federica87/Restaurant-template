9f3d2dd0536d271393920c924ce581d890d65b68
Alessia Russo <r.alessia2494@gmail.com>
2022-04-15 18:53:07 +0200

    add box, texts and img

---
+++ src/app/components/section2/sec2-img/sec2-img.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sec2-img',
+  templateUrl: './sec2-img.component.html',
+  styleUrls: ['./sec2-img.component.css']
+})
+export class Sec2ImgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
