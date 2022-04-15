b30677f318156553882e5b5f750e92ecd25b5529
Alessia Russo <r.alessia2494@gmail.com>
2022-04-15 16:07:14 +0200

    impostazioniiniziali

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Sec2ContainerComponent } from './components/section2/sec2-container/sec2-container.component';
+import { Sec2ContentComponent } from './components/section2/sec2-content/sec2-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Sec2ContainerComponent,
+    Sec2ContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/section2/sec2-container/sec2-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sec2-container',
+  templateUrl: './sec2-container.component.html',
+  styleUrls: ['./sec2-container.component.css']
+})
+export class Sec2ContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
